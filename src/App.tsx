72f832a2d2f2887a28fcd8b13259d3c71f1634f8
raphpay <raphpay.dev@gmail.com>
2025-08-09 16:42:33 +0400

    Simple call to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
 import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const BACKEND_URL = import.meta.env.VITE_BACK_END_URL;
+  const [message, setMessage] = useState<string>("");
+
+  async function callBackend() {
+    try {
+      const res = await fetch(`${BACKEND_URL}/test`);
+      console.log("res", BACKEND_URL, res);
+      const text = await res.text();
+      setMessage(text);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="p-4">
+      <button
+        onClick={callBackend}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Test
+      </button>
+      {message && <p className="mt-4 text-lg">{message}</p>}
+    </div>
   );
 }
 
