153fd72e5b508d78d1537ee813fffb1055b95afe
raphpay <raphpay.dev@gmail.com>
2025-08-09 16:57:59 +0400

    Add login to strava

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
   const BACKEND_URL = import.meta.env.VITE_BACK_END_URL;
-  const [message, setMessage] = useState<string>("");
 
-  async function callBackend() {
-    try {
-      const res = await fetch(`${BACKEND_URL}/test`);
-      console.log("res", BACKEND_URL, res);
-      const text = await res.text();
-      setMessage(text);
-    } catch (error) {
-      console.error("Error:", error);
-    }
+  async function connectStrava() {
+    window.location.href = `${BACKEND_URL}/auth/login`;
   }
 
   return (
     <div className="p-4">
       <button
-        onClick={callBackend}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={connectStrava}
+        className="bg-orange-500 text-white px-4 py-2 rounded"
       >
-        Test
+        Se connecter à Strava
       </button>
-      {message && <p className="mt-4 text-lg">{message}</p>}
     </div>
   );
 }
